91191b28233c3bc04d07b25c7efd6fc0997c40d4
Sharanya-Bharghavi <ssharanyab@gmail.com>
2020-07-14 18:04:36 +0530

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+import 'home_page.dart';
+
+void main()=>runApp(new MyApp());
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return new MaterialApp(
+      theme: new ThemeData(primaryColor: Colors.blue[900]),
+      home: new HomePage(),
+    );
+  }
+}
